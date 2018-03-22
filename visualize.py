65e3276d9a834730d35593666ef3e99e292a4701
brian <briangeoghegan2010@gmail.com>
2018-03-22 21:04:38 +0000

    MlIIAverage

--- visualize.py
+++ visualize.py
@@ -1,6 +1,9 @@
 
 import matplotlib.pyplot as plt
-from scipy.signal import butter, lfilter
+import math
+
+import time
+from scipy import signal
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
@@ -15,15 +18,40 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=3):
     y = lfilter(b, a, data)
     return y
 
+def bandPassFilter(originalSignal):
+    startFilter = time.time()
+    filterOrder = 4;
+    lowCutOffFreq = 0.25
+    highCutOffFreq = 0.01
+    B, A = signal.butter(filterOrder, highCutOffFreq, btype='high', output='ba')
+    filteredSignal = signal.filtfilt(B, A, originalSignal, axis=0);
+    B, A = signal.butter(filterOrder, lowCutOffFreq, btype='low', output='ba')
+    filteredSignal = signal.filtfilt(B, A, filteredSignal, axis=0);
+    endFilter = time.time()
+    print("Filter Elapsed time", endFilter - startFilter, "Filter order: ", filterOrder)
+    return filteredSignal
+
+def get_change(current, previous):
+    if current == previous:
+        return 100.0
+    try:
+        measurement = abs(current - previous)/previous
+        percent = measurement*100
+        return percent
+    except ZeroDivisionError:
+         return 0
+
+
 #Data sampled at 360/second
 samplingFreq = 360
 
 text_file = open("data.txt", "r")
 lines = text_file.readlines()
 
-dataArray = { "time": [], "mlII":[], "v5": [] }
+dataArray = { }
 
-counter = 0;
+
+
 #get values and time for each line
 for x in lines:
     x = x.strip().replace("\t","")
@@ -31,12 +59,26 @@ for x in lines:
     for x in tempval:
         if x == "":
             tempval.remove(x)
-    dataArray["v5"].append(float(tempval[2]))
-    dataArray["time"].append(float(tempval[0]))
-    dataArray["mlII"].append(float(tempval[1]))
-    counter = counter + 1
+    time = tempval[0]
+    mlIIvalue = tempval[1]
+    dataArray.update({str(time): float(mlIIvalue)})
 
-dataArray["mlII"] = butter_bandpass_filter(dataArray["mlII"], 0.5, 2, samplingFreq, 3)
+#Normalized value of mlII
+mlIIAverageValue = math.sqrt(sum(dataArray.values())*sum(dataArray.values())) / len(dataArray)
+mlII_R_peaks = { }
 
-plt.plot(dataArray["time"][:3000], dataArray["v5"][:3000])
+#Begin feature extraction from mlII
+for key, value in dataArray.items():
+    currReading = value
+    if(currReading > mlIIAverageValue):
+        mlII_R_peaks.update({str(key): float(value)})
+
+print(mlII_R_peaks)
+#for time in dataArray["time"]:
+
+#dataArray["mlII"] = bandPassFilter(dataArray["mlII"])
+samplesToPlot = 1000
+plt.plot(dataArray["time"][:samplesToPlot], dataArray["mlII"][:samplesToPlot])
+
+
 plt.show()
